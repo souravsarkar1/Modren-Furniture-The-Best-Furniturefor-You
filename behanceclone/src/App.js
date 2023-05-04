35d829f9f6bdadf7015773e412a86e4390a1ce81
PremShakti <premshakti.ps@gmail.com>
2023-05-04 22:05:37 +0530

    s

--- behanceclone/src/App.js
+++ behanceclone/src/App.js
@@ -4,7 +4,6 @@ import MainRoutes from './Pages/MainRoutes';
 function App() {
   return (
     <div className="App">
-      <h1>Har Har Mahadev</h1>
       <MainRoutes/>
     </div>
   );
--- behanceclone/src/Pages/MainRoutes.jsx
+++ behanceclone/src/Pages/MainRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from './Login'
+import Home from './Home'
 
 const MainRoutes = () => {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Home/>} />
       <Route path='/login' element={<Login/>}/>
       </Routes>
     </div>
